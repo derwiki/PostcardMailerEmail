1deb4a0cda03993ee457586328c87ab07afb5069
Adam Derewecki <a@der.wiki>
2025-04-22 23:49:20 -0700

    Fix headers

--- app/services/sendgrid_post_handler.rb
+++ app/services/sendgrid_post_handler.rb
@@ -12,7 +12,7 @@ class SendgridPostHandler
   def process
     Rails.logger.info "SendgridPostHandler params: #{@params}"
     Rails.logger.info "SendgridPostHandler params.keys: #{@params.keys}"
-    Rails.logger.info "SendgridPostHandler Authentication-Results: #{@params[:headers]&.dig('Authentication-Results')}"
+    Rails.logger.info "SendgridPostHandler headers: #{@params[:headers]}"
 
     bodytext = @params[:text]
     @from_email = extract_email_from_sendgrid_from(@params[:from])
