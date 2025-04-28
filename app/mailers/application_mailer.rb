c8eff1216dcc1f12808cec0f421cc8532b2fc147
Adam Derewecki <a@der.wiki>
2025-04-28 12:02:07 -0400

    Potentially fix 'email clipped' issue again

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "PostcardMailer.us <notifications@postcardmailer.us>"
   layout "email"
+
+  private
+
+  def set_content_type
+    headers["Content-Type"] = "text/html; charset=UTF-8"
+  end
 end
