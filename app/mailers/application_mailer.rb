9b209c76ac9fbe7421837ad861a19a421c54a424
Adam Derewecki <a@der.wiki>
2025-04-28 22:52:57 -0400

    Remove set_content_type

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,12 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "PostcardMailer.us <notifications@postcardmailer.us>"
   layout "email"
-
-  before_action :set_content_type
-
-  private
-
-  def set_content_type(*args)
-    headers["Content-Type"] = "text/html; charset=UTF-8"
-  end
 end
