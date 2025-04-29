dbac2caa2af523708d1f6691712c7ef0e6cedb67
Adam Derewecki <a@der.wiki>
2025-04-28 22:49:22 -0400

    Clean up email partial styling

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,9 +2,11 @@ class ApplicationMailer < ActionMailer::Base
   default from: "PostcardMailer.us <notifications@postcardmailer.us>"
   layout "email"
 
+  before_action :set_content_type
+
   private
 
-  def set_content_type
+  def set_content_type(*args)
     headers["Content-Type"] = "text/html; charset=UTF-8"
   end
 end
